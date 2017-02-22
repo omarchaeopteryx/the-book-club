25fd2a6416e69185cc23d639bdadcdf01f3c99b0
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-22 11:04:39 -0800

    Fix search for differentiating between 1 and many results

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,20 @@
 class WelcomeController < ApplicationController
   def index
     client = Goodreads::Client.new(api_key: ENV['GR_KEY'], api_secret: ENV['GR_SECRET'])
-    @search2 = client.book("18512") # Example book: LOTR by Goodreads ID.
+    # @search2 = client.book("18512") # <--Text example: LOTR by Goodreads ID.
   end
 
   def results
     client = Goodreads::Client.new(api_key: ENV['GR_KEY'], api_secret: ENV['GR_SECRET'])
-    @search1 = client.search_books(params[:search]) # Refactor so user input dictates this.
+
+    @search_raw = client.search_books(params[:search]) # Refactor so user input dictates this.
+
+    # NOTE: the number of parameters per book is 9; therefore view page criterion is 9; CHECK for updates is data structure changes.
+    
+    # if @search_raw.results.count == 1
+    #   @search_unique = @search_raw
+    # end
+
     render :_results
   end
 
