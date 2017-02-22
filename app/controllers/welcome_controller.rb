da625e5e7add533bdeb13e898b608be0c3b30c0c
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-22 14:15:36 -0800

    Fix and update search bar for styling, edge cases

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,17 +7,20 @@ class WelcomeController < ApplicationController
   def results
     client = Goodreads::Client.new(api_key: ENV['GR_KEY'], api_secret: ENV['GR_SECRET'])
 
-    @search_raw = client.search_books(params[:search]) # Refactor so user input dictates this.
+    @search_raw = client.search_books(params[:search])
 
-    # NOTE: the number of parameters per book is 9; therefore view page criterion is 9; CHECK for updates is data structure changes.
+    if @search_raw.query == "" || @search_raw.results_end == "0"
+      # Checking for edge cases.
+      @error_message_for_search = "Alas, dear user! Please try again"
 
-    if @search_raw.results.first[1].count == 9
+    elsif @search_raw.results.first[1].count == 9
+      # NOTE: the number of parameters per book is 9; therefore view page criterion is 9; CHECK for updates is data structure changes.
       @unique_result = @search_raw
+
     else
       @multi_results = @search_raw
-    end
 
-    # p @search_raw.results.first
+    end
 
     render :_results
   end
