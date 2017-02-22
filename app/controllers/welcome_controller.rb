11b32b05269eeb79f97cffdac9dea850ae1ae781
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-21 17:37:58 -0800

    Add basic search functionality; add Goodreads API

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+    client = Goodreads::Client.new(api_key: ENV['GR_KEY'], api_secret: ENV['GR_SECRET'])
+    @search2 = client.book("18512") # Example book: LOTR by Goodreads ID.
   end
+
+  def results
+    client = Goodreads::Client.new(api_key: ENV['GR_KEY'], api_secret: ENV['GR_SECRET'])
+    @search1 = client.search_books(params[:search]) # Refactor so user input dictates this.
+    render :_results
+  end
+
 end
