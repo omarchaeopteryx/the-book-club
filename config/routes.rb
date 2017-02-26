c27d18e9274afce6a81cb94f582c0a03bf78636e
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-25 17:41:33 -0800

    Add devise registration routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+
   get 'welcome/index'
   root 'welcome#index'
   get '/welcome/results', to:'welcome#results'
   resources :topics do
     resources :comments
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
