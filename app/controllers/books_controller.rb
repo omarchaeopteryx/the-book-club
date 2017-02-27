4a49b8c1b28ee2588ee845b8a5a9969133ec1e1f
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-27 14:53:17 -0800

    Fix author display on book ID page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,14 @@ class BooksController < ApplicationController
     client = Goodreads::Client.new(api_key: ENV['GR_KEY'], api_secret: ENV['GR_SECRET'])
     @book_search_result = client.book(params[:id])
 
+    @book_search_result_author = ""
+
+    if @book_search_result.authors.first[1][0] != nil
+      @book_search_result_author = @book_search_result.authors.first[1][0].name
+    elsif @book_search_result.authors.first[1][0] == nil
+      @book_search_result_author = @book_search_result.authors.first[1].name
+    end
+    # FIX: What if there are mulitple authors of books?
     # p @book_search_result <--Debugger
   end
 
