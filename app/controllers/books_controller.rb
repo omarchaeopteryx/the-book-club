c8811271fba43d3dd9d7d7f0e6daa7f97573bd66
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-27 10:42:09 -0800

    Add book text, image, author, description to show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,9 @@ class BooksController < ApplicationController
 
   def show
     client = Goodreads::Client.new(api_key: ENV['GR_KEY'], api_secret: ENV['GR_SECRET'])
-    @book_search_result = client.book(params[:id]) # <--Text example: LOTR by Goodreads ID.
+    @book_search_result = client.book(params[:id])
+
+    # p @book_search_result <--Debugger
   end
 
 end
