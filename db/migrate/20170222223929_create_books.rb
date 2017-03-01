33b303b8d77cbad6301755d3e0e9a4bf44f43224
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-28 21:06:10 -0800

    Remove extra btree reference for Heroku deploy fix

--- db/migrate/20170222223929_create_books.rb
+++ db/migrate/20170222223929_create_books.rb
@@ -6,6 +6,6 @@ class CreateBooks < ActiveRecord::Migration[5.0]
       t.timestamps
     end
 
-    add_index "books", ["goodreads_id"], name: "index_books_on_goodreads_id", unique: true, using: btree
+    add_index "books", ["goodreads_id"], name: "index_books_on_goodreads_id", unique: true # using: btree
   end
 end
